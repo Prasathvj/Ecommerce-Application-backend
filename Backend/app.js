44109c6560c9e0cd9a5e787e6d5051cb40fbdebe
Prasathvj <prasathvj18@gmail.com>
2023-07-07 11:08:05 +0530

    error checking

--- Backend/app.js
+++ Backend/app.js
@@ -5,10 +5,12 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 const dotenv = require('dotenv');
 dotenv.config({path:path.join(__dirname,"config/.env")});
+const cors =require("cors")
 
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
 
 const products = require('./routes/product')
@@ -20,13 +22,12 @@ app.use('/api/v1/',products);
 app.use('/api/v1/',auth);
 app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
-
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-    })
-}
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')));
+//     app.get('*', (req, res) =>{
+//         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+//     })
+// }
 
 app.use(errorMiddleware)
 
