d6436d22365a2ff70413e7fec73e5673ab4b1216
Prasathvj <prasathvj18@gmail.com>
2023-07-11 16:37:31 +0530

    nodemail update

--- Backend/utils/email.js
+++ Backend/utils/email.js
@@ -1,25 +1,23 @@
 const nodemailer = require('nodemailer')
 
 const sendEmail = async options => {
-    const transport = {
-        host:process.env.SMTP_HOST,
-         port: process.env.SMTP_PORT,
+
+     // Send the password reset link to the user's email
+     const transporter = nodemailer.createTransport({
+        // Configure your email provider here
+        service: 'gmail',
         auth: {
-            user:process.env.SMTP_USER,
-            pass:process.env.SMTP_PASS
-    }
-    };
-
-    const transporter = nodemailer.createTransport(transport);
-
-    const message = {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
+        },
+      });
+      const mailOptions = {
         from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
         to: options.email,
-        subject: options.subject,
+        subject: 'VJ-MART Password Reset',
         text: options.message
-    }
-
-   await transporter.sendMail(message)
+      };
+     await transporter.sendMail(mailOptions);
 }
 
 module.exports = sendEmail
